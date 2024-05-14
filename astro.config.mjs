578825d7c21f858d33337c90182f7d7be7428302
Gareth Dwyer <sixhobbits@users.noreply.github.com>
2024-05-14 16:45:04 +0200

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'EMEA Entrepreneurs',
+			social: {
+				github: 'https://github.com/emeatech/',
+			},
+			sidebar: [
+				{
+					label: 'Anti advice',
+					autogenerate: { directory: 'anti' },
+				},
+			],
+		}),
+	],
+});
